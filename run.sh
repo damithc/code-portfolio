b26bb34af275a63643d9b592a4a88a0146965f34
damithc <1673303+damithc@users.noreply.github.com>
2025-05-02 19:30:11 +0800

    Add start date

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --portfolio
+java -jar RepoSense.jar --portfolio --since 05/01/2024
